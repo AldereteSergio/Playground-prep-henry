6cbf028c21f757e2a658b9b5326e34dfc53748c1
AldereteSergio <Alderete.sergio.j@gmail.com>
2023-05-05 17:34:54 -0300

    add playground arrays

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var listaDeCompras = [];
+
+listaDeCompras[3] = 'Tomates';
+listaDeCompras[1] = 'Lechuga';
+
+//console.log(listaDeCompras);
+console.log(listaDeCompras[0]);
+
+//var colores = ['amaillo', 'azul'];
+//colores.push('rojo');
+//colores.unshift('verde');
+//colores.pop();
+//colores.shift();
+//console.log(colores);
+
+
+//var pintores = ["Picasso", "Velazquez", "Van Gogh", "Dali"];
+//var incluyeDali = pintores.includes('Monet ');
+//console.log(incluyeDali);
+
+//var numeros = [10, 6, 8, 9];
+//var cumplenCondicion = numeros.every(num => {
+//    return num > 5;
+//});
+//console.log(cumplenCondicion);
+
